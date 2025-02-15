e750455caab077df208f56ff1f329ad9ca467684
Toby Smith <tobysmith568@hotmail.co.uk>
2025-02-15 16:01:39 +0000

    Configure @typescript-eslint/no-unused-vars

--- src/configs/base.config.ts
+++ src/configs/base.config.ts
@@ -20,6 +20,22 @@ export const baseConfig: Linter.Config[] = [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat["jsx-runtime"],
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true
+        }
+      ]
+    }
+  },
 
   // Unit test plugins
   {
