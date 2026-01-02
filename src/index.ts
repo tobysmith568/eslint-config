d969396fcc2e46a82003c1c57283e8be83acc10e
Toby Smith <tobysmith568@hotmail.co.uk>
2026-01-02 00:16:32 +0000

    Fix types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import type { Linter } from "eslint";
 import { baseConfig } from "./configs/base.config.js";
 import { nextConfig } from "./configs/next.config.js";
 
-export default {
+type Configs = {
+  recommended: Linter.Config[];
+  next: (dirname: string) => Linter.Config[];
+};
+
+const configs: Configs = {
   recommended: baseConfig,
   next: nextConfig
 };
+
+export default configs;
